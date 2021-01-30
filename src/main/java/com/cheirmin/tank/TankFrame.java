2b6629d9205f1b999d073f7c781f9cfcee7ae882
cheirmin <1147905069@qq.com>
2021-01-30 13:28:41 +0800

    【新增】键盘监听事件

--- src/main/java/com/cheirmin/tank/TankFrame.java
+++ src/main/java/com/cheirmin/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.cheirmin.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -13,16 +15,19 @@ import java.awt.event.WindowEvent;
  */
 
 public class TankFrame extends Frame {
+    int x = 200, y = 200;
+
     public TankFrame() {
         //大小
-        setSize(800,600);
+        setSize(800, 600);
         //固定大小
         setResizable(false);
         //标题
         setTitle("坦克大战");
-
         //可见
         setVisible(true);
+
+        this.addKeyListener(new MyKeyListener());
 
         //窗口可关闭
         addWindowListener(new WindowAdapter() {
@@ -35,11 +40,34 @@ public class TankFrame extends Frame {
 
     /**
      * 窗口绘制的时候，会调用这个
+     *
      * @param g 画笔
      */
     @Override
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
+//        System.out.println("paint");
         //绘制一个矩形
-        g.fillRect(200,200,50,50);
+        g.fillRect(x, y, 50, 50);
+    }
+
+    /**
+     * 键盘监听类
+     */
+    class MyKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            //按键按下
+//            System.out.println("key Pressed");
+            x += 30;
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            //按键松开
+//            System.out.println("key Released");
+            y += 30;
+            repaint();
+        }
     }
 }
